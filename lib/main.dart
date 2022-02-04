3e4f7c553b17c7f98106c9389a5f477f5968f765
Matheus <matheusilva-m@outlook.com>
2022-02-03 23:35:36 -0300

    small changes at home page and its components

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/ui/web/pages/home_page/home_page.dart';
 
-import 'ui/web/pages/home_page/home_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Aparicio's Portfolio",
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
--- lib/ui/web/pages/home_page/home_page.dart
+++ lib/ui/web/pages/home_page/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/ui/web/pages/home_page/components/home_app_bar_component.dart';
+import 'package:portfolio/ui/web/pages/home_page/components/home_body_component.dart';
+import 'package:portfolio/ui/web/pages/home_page/components/home_bottom_bar_component.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -10,8 +13,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return const Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: HomeAppBarComponent(),
+      ),
+      body: HomeBodyComponent(),
+      bottomNavigationBar: HomeBottomBarComponent(),
     );
   }
 }
